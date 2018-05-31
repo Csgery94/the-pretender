d35e336883e6ef481226c374dc11e6dcea329aad
David Koczka <koczkadavid@gmail.com>
2018-05-31 20:45:01 +0200

    push

---
+++ src/components/Pretender.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Pretender extends Component {
+  render () {
+    return (
+      <div>
+        Ezittagame
+      </div>
+    )
+  }
+}
